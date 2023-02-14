e485ef135f97cb941f5e7926e750f60a1a5202e1
Sinnamkushwah <122508948+Sinnamkushwah@users.noreply.github.com>
2023-02-14 20:03:25 +0530

    Updated Domain classes

--- src/production/Agumento.Core.Domain/Account.cs
+++ src/production/Agumento.Core.Domain/Account.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Agumento.Core.Domain
 {
     public class Account : BaseEntity
-    {       
-        public string? AccountId { get; set; }        
-        public string? AccountName { get; set; }
+    {
+        [Required]
+        public string AccountId { get; set; }
+
+        [Required]
+        public string AccountName { get; set; }
         public string? AccountDetails { get; set; }
-        public string? AccountManager { get; set; }
+
+        [Required]
+        public string AccountManager { get; set; }
     }
 }
--- src/production/Agumento.Core.Domain/Project.cs
+++ src/production/Agumento.Core.Domain/Project.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Agumento.Core.Domain
 {
     public class Project : BaseEntity
-    {  
+    {
         [Required]
-        public string? ProjectName { get; set; }
+        public string ProjectId { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
 
         public string? ProjectDetails { get; set; }
 
         [Required]
-        public string? ProjectManager { get; set; }
+        public string ProjectManager { get; set; }
 
         [Display(Name = "Account")]
         public virtual Guid AccountId { get; set; }
 
-        [ForeignKey("AccountId")]
+        [ForeignKey("Id")]
         public virtual Account Account { get; set; }
     }
 }
