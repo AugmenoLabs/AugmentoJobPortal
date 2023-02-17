3545cf39a0cafbbc36518c5e1cb57094ab0b0d0b
Sinnamkushwah <122508948+Sinnamkushwah@users.noreply.github.com>
2023-02-17 10:43:54 +0530

    Updated account by AccountId

--- src/production/Agumento.Core.Application/Features/AccountFeatures/Queries/GetAccountByIdQuery.cs
+++ src/production/Agumento.Core.Application/Features/AccountFeatures/Queries/GetAccountByIdQuery.cs
@@ -16,7 +16,16 @@ namespace Agumento.Core.Application.Features.AccountFeatures.Queries
             }
             public async Task<Account> Handle(GetAccountByIdQuery query, CancellationToken cancellationToken)
             {
-                var account = _context.Accounts.Where(a => a.Id == query.Id).FirstOrDefault();
+               // var account = _context.Accounts.Where(a => a.Id == query.Id).FirstOrDefault();
+                var account = _context.Accounts.Where(a => a.Id == query.Id).Select(a => new Account()
+                {
+                    Id = a.Id,
+                    AccountId = a.AccountId,
+                    AccountName = a.AccountName,
+                    AccountDetails = a.AccountDetails,
+                    AccountManager = a.AccountManager,
+                    Projects = a.Projects.ToList()
+                }).FirstOrDefault();
 
                 if (account == null) return null;
                 return account;
