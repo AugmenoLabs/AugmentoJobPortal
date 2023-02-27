56d3814dd42b9982bbe554b1bb5f450b765a528b
Sinnamkushwah <122508948+Sinnamkushwah@users.noreply.github.com>
2023-02-27 16:49:38 +0530

    Candidatefeedback

--- src/production/Agumento.Core.Application/Features/CandidateProfileFeatures/Queries/GetAllCandidateScheduleProfileQuery.cs
+++ src/production/Agumento.Core.Application/Features/CandidateProfileFeatures/Queries/GetAllCandidateScheduleProfileQuery.cs
@@ -17,37 +17,36 @@ namespace Agumento.Core.Application.Features.CandidateProfileFeatures.Queries
             }
             public async Task<IEnumerable<response.CandidateProfileSchedule>> Handle(GetAllCandidateScheduleProfileQuery query, CancellationToken cancellationToken)
             {
-                var candidateProfileScheduls = await (from c in _context.CandidateProfiles
-                                                      join v in _context.Vendors on c.VendorId equals v.Id
-                                                      join i in _context.ScheduleInterviews on c.Id equals i.CandidateId
-                                                      // join f in _context.CandidateInterviews on c.Id equals f.CandidateId
-                                                      select new response.CandidateProfileSchedule
-                                                      {
-                                                          CandidateName = c.CandidateName,
-                                                          Email = c.Email,
-                                                          ContactNumber = c.ContactNumber,
-                                                          ResidentialAddress = c.ResidentialAddress,
-                                                          PermanenetAddress = c.PermanenetAddress,
-                                                          Gender = c.Gender,
-                                                          MaritalStatus = c.MaritalStatus,
-                                                          YearOfExperience = c.YearOfExperience,
-                                                          PrimarySkills = c.PrimarySkills,
-                                                          SecondarySkills = c.SecondarySkills,
-                                                          Qualification = c.Qualification,
-                                                          OpenPositionId = c.OpenPositionId,
-                                                          VendorId = c.VendorId,
-                                                          VendorName = v.VendorName,
-                                                          NoticePeriod = c.NoticePeriod,
-                                                          ScheduleDate = i.ScheduledTimeFrom,
-                                                          InterviewerName = i.InterviewerName,
-                                                          HasOfferLetter = c.HasOfferLetter,
-                                                          Resume = c.Resume,
-                                                          FileName = c.FileName,
-                                                          FileExt = c.FileExt,
-                                                          CandidateFeedbacks = c.CandidateInterviews
+                var liqQuery = from c in _context.CandidateProfiles
+                               join v in _context.Vendors on c.VendorId equals v.Id
+                               join i in _context.ScheduleInterviews on c.Id equals i.CandidateId
+                               select new response.CandidateProfileSchedule
+                               {
+                                   CandidateName = c.CandidateName,
+                                   Email = c.Email,
+                                   ContactNumber = c.ContactNumber,
+                                   ResidentialAddress = c.ResidentialAddress,
+                                   PermanenetAddress = c.PermanenetAddress,
+                                   Gender = c.Gender,
+                                   MaritalStatus = c.MaritalStatus,
+                                   YearOfExperience = c.YearOfExperience,
+                                   PrimarySkills = c.PrimarySkills,
+                                   SecondarySkills = c.SecondarySkills,
+                                   Qualification = c.Qualification,
+                                   OpenPositionId = c.OpenPositionId,
+                                   VendorId = c.VendorId,
+                                   VendorName = v.VendorName,
+                                   NoticePeriod = c.NoticePeriod,
+                                   ScheduleDate = i.ScheduledTimeFrom,
+                                   InterviewerName = i.InterviewerName,
+                                   HasOfferLetter = c.HasOfferLetter,
+                                   Resume = c.Resume,
+                                   FileName = c.FileName,
+                                   FileExt = c.FileExt,
+                                   CandidateFeedbacks = _context.CandidateInterviews.Where(a => a.CandidateId.Equals(c.Id)).ToList(),
 
-                                                      }).ToListAsync();
-                //var aa = _context.CandidateProfiles.Select(p => new { p.Id, p.CandidateInterviews }).ToList();
+                               };
+                var candidateProfileScheduls = await (liqQuery).ToListAsync();
 
                 return candidateProfileScheduls;
             }
