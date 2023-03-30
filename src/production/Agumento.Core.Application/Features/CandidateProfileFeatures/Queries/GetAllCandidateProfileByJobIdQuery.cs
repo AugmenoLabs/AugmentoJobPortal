0ef73cf698e3d63eafd6cabe836deb42e7f19163
Sinnamkushwah <122508948+Sinnamkushwah@users.noreply.github.com>
2023-03-30 10:36:49 +0530

    Expose CandidateProfileByOpenPositionId

---
+++ src/production/Agumento.Core.Application/Features/CandidateProfileFeatures/Queries/GetAllCandidateProfileByJobIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using Agumento.Core.Application.Interfaces;
+using Agumento.Core.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agumento.Core.Application.Features.CandidateProfileFeatures.Queries
+{
+    public class GetAllCandidateProfileByJobIdQuery : IRequest<IEnumerable<CandidateProfile>>
+    {
+        public Guid OpenPositionId { get; set; }
+        public class GetAllCandidateProfileByJobIdQueryHandler : IRequestHandler<GetAllCandidateProfileByJobIdQuery, IEnumerable<CandidateProfile>>
+        {
+            private readonly IApplicationDbContext _context;
+            public GetAllCandidateProfileByJobIdQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<IEnumerable<CandidateProfile>> Handle(GetAllCandidateProfileByJobIdQuery query, CancellationToken cancellationToken)
+            {
+                var candidateProfileList = await _context.CandidateProfiles.Where(a => a.OpenPositionId == query.OpenPositionId).ToListAsync();
+
+                if (candidateProfileList == null)
+                {
+                    return null;
+                }
+                return candidateProfileList.AsReadOnly();
+            }
+
+        }
+    }
+}
+
