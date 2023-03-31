b3530ccf29b2e58d331b5b2041b205be7fc8745b
sraghavendra94 <sasula.ragu@gmail.com>
2023-03-31 12:54:59 +0530

    word preview

--- src/production/Agumento.Core.Application/Features/CandidateProfileFeatures/Commands/CreateCandidateProfileCommand.cs
+++ src/production/Agumento.Core.Application/Features/CandidateProfileFeatures/Commands/CreateCandidateProfileCommand.cs
@@ -1,7 +1,10 @@
 ﻿using Agumento.Core.Application.Interfaces;
 using Agumento.Core.Domain;
+using Aspose.Words;
 using AutoMapper;
 using MediatR;
+using System.IO;
+using Document = Aspose.Words.Document;
 
 namespace Agumento.Core.Application.Features.CandidateProfileFeatures.Commands
 {
@@ -42,6 +45,8 @@ namespace Agumento.Core.Application.Features.CandidateProfileFeatures.Commands
             }
             public async Task<Guid> Handle(CreateCandidateProfileCommand request, CancellationToken cancellationToken)
             {
+                try
+                {
                     //CandidateProfile candidateProfile = _mapper.Map<CandidateProfile>(request);
                     var candidateProfile = new CandidateProfile();
 
@@ -64,8 +69,14 @@ namespace Agumento.Core.Application.Features.CandidateProfileFeatures.Commands
                     candidateProfile.HasOfferLetter = request.HasOfferLetter;
                     candidateProfile.FileName = request.FileName;
                     candidateProfile.FileExt = request.FileExt;
-
                     candidateProfile.Resume = request.Resume;
+
+                    if (request.FileExt == "docx")
+                    {
+                        candidateProfile.Resume = WordToPdf(request.Resume);
+                        candidateProfile.FileExt = "pdf";
+                        candidateProfile.FileName = "Resume.pdf";
+                    }
 
 
                     _context.CandidateProfiles.Add(candidateProfile);
@@ -73,6 +84,25 @@ namespace Agumento.Core.Application.Features.CandidateProfileFeatures.Commands
 
                     return candidateProfile.Id;
                 }
+                catch(Exception ex)
+                {
+                    return Guid.NewGuid();
+                }
+            }
+
+            private static Byte[] WordToPdf(byte[] resume)
+            {
+                // This represents the input byte array
+                byte[] docBytes = resume;
+                // Load Word Document from this byte array
+                Document loadedFromBytes = new(new MemoryStream(docBytes));
+                // Save Word to PDF byte array
+                MemoryStream pdfStream = new();
+                loadedFromBytes.Save(pdfStream, SaveFormat.Pdf);
+                byte[] pdfBytes = pdfStream.ToArray();
+                return pdfBytes;
+
+            }
         }
     }
 }
